6c489234a6d14d7370057dc5db392f355495505a
adamking49169 <adamking49169@gmail.com>
2025-07-17 21:45:35 +0100

    more changes

---
+++ src/FitnessTracker.Infrastructure/INutritionAggregatorService.cs
@@ -0,0 +1,9 @@
+﻿using FitnessTracker.Core.Models;
+
+namespace FitnessTracker.Infrastructure.Services;
+
+public interface INutritionAggregatorService
+{
+    Task<FoodEntry> GetMacrosForBarcodeAsync(string barcode, Guid userId);
+    Task<FoodEntry> CreateManualEntryAsync(double protein, double carbs, double fat, Guid userId);
+}
