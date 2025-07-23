544a95018cee859d3e9b6a77d8805d0b6a669236
adamking49169 <adamking49169@gmail.com>
2025-07-23 19:34:34 +0000

    Add migration for user foreign key on WeightEntry

---
+++ src/FitnessTracker.Infrastructure/Migrations/20250723193211_AddWeightEntryUserFk.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWeightEntryUserFk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_WeightEntries_UserId",
+                table: "WeightEntries",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeightEntries_Users_UserId",
+                table: "WeightEntries",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeightEntries_Users_UserId",
+                table: "WeightEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WeightEntries_UserId",
+                table: "WeightEntries");
+        }
+    }
+}
